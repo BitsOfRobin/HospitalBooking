1f79d9c6748a0f054a0c0e7f24b06c2298fc3f2a
boscojekyll888 <wongrj-pm21@student.tarc.edu.my>
2023-04-01 16:09:39 +0800

    I have done Feedback and Comment part.

--- app/src/main/java/com/example/hospitalbooking/BookingAppointment/Feedback.kt
+++ app/src/main/java/com/example/hospitalbooking/BookingAppointment/Feedback.kt
@@ -22,6 +22,7 @@ class Feedback : AppCompatActivity()  {
         return super.onSupportNavigateUp()
     }
 
+
     private fun appointmentDetail() {
         val docName = intent.getStringExtra("DoctorName")
         val userAppointment = intent.getStringExtra("Appointment")
