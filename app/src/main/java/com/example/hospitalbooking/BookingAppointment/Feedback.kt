15f153dfcc1d2ac7800cc997fc734c216621eb45
boscojekyll888 <wongrj-pm21@student.tarc.edu.my>
2023-04-06 16:35:21 +0800

    I have done Feedback and Comment part.

--- app/src/main/java/com/example/hospitalbooking/BookingAppointment/Feedback.kt
+++ app/src/main/java/com/example/hospitalbooking/BookingAppointment/Feedback.kt
@@ -1,24 +1,43 @@
 package com.example.hospitalbooking.BookingAppointment
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hospitalbooking.R
+import com.example.hospitalbooking.databinding.ActivityAppointmentFeedbackBinding
+import com.google.firebase.firestore.FirebaseFirestore
 import org.w3c.dom.Text
 
 class Feedback : AppCompatActivity()  {
+    private lateinit var binding : ActivityAppointmentFeedbackBinding
+    private val db : FirebaseFirestore = FirebaseFirestore.getInstance()
+    private lateinit var docName :String
+    private lateinit var userAppointment :String
+    private lateinit var userName :String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_appointment_feedback)
+        binding = ActivityAppointmentFeedbackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_appointment_feedback)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setTitle("Comment Appointment")
 
         appointmentDetail()
-        appointmentRating()
-        appointmentComment()
-        textComment()
+//        appointmentRating()
+//        appointmentComment()
+//        textComment()
+
+        //val submit = findViewById<Button>(R.id.submitComment)
+        binding.submitComment.setOnClickListener {
+            appointmentDetail()
+            appointmentRating()
+            appointmentComment()
+            textComment()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -27,9 +46,9 @@ class Feedback : AppCompatActivity()  {
     }
 
     private fun appointmentDetail() {
-        val docName = intent.getStringExtra("DoctorName")
-        val userAppointment = intent.getStringExtra("Appointment")
-        val userName = intent.getStringExtra("userName")
+        docName = intent.getStringExtra("DoctorName").toString()
+        userAppointment = intent.getStringExtra("Appointment").toString()
+        userName = intent.getStringExtra("userName").toString()
 
         val doctor = findViewById<TextView>(R.id.doctorName)
         val appointment = findViewById<TextView>(R.id.appointment)
@@ -47,11 +66,46 @@ class Feedback : AppCompatActivity()  {
     }
 
     private fun appointmentComment() {
+        // Rating Star
+        val rating = findViewById<RatingBar>(R.id.ratingComment)
+        val ratingStar = rating.rating
+
+        // Radio Question
         val radioGroup = findViewById<RadioGroup>(R.id.questionOneRadio)
         val radioGroup2 = findViewById<RadioGroup>(R.id.questionTwoRadio)
 
         var answer1 = buttonGroupSelection(radioGroup)
         var answer2 = buttonGroupSelection2(radioGroup2)
+
+        // Comment
+        val comment = findViewById<TextView>(R.id.commentText)
+        var commentText = comment.text.toString()
+        Toast.makeText(this, "Your comment: ${comment.text.toString()}", Toast.LENGTH_SHORT).show()
+
+        val feedback = hashMapOf(
+            "docName" to docName,
+            "doctorAppoint" to userAppointment,
+            "user" to userName
+        )
+
+        val feedbackFirebase = hashMapOf(
+            "docName" to docName,
+            "doctorAppoint" to userAppointment,
+            "user" to userName,
+            "rateStar" to ratingStar,
+            "radioAns1" to answer1,
+            "radioAns2" to answer2,
+            "comment" to commentText
+        )
+
+        db.collection("userAppointment").document(feedback.toString()).collection("comment").document(feedbackFirebase.toString())
+            .set(feedbackFirebase)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Thank you for your feedback.", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "The feedback failed to submit please try again.", Toast.LENGTH_SHORT).show()
+            }
 
 
 
